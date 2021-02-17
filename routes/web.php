37ad946a05f1954db2cba360474bc84d7295e4c1
Star-Gibson <sgibsoncoding@gmail.com>
2021-02-17 14:19:21 -0500

    Understanding Wildcards

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//Understanding use of wildcards
 Route::get('/posts/{post}', function ($post) {
-$posts = [
-    'my-first-post' => 'Hello, this is my first blog post!',
-    'my-second-post' => "Now I am getting the hang of this blogging thing."
-];
-
-    return view ('posts', [
+    //Currently no database, using datastore
+    $posts = [
+        'my-first-post' => 'Hello, this is my first blog post!',
+        'my-second-post' => "Now I am getting the hang of this blogging thing."
+    ];
+    //Return 404 if no posts are associated with route.
+    if (!array_key_exists($post, $posts)) {
+        abort(404, 'SOrry this post was not found.');
+    }
+    //Return Posts View
+    return view('posts', [
         'post' => $posts[$post]
     ]);
 });
